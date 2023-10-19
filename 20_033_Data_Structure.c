8e7f3e77cf8f2ef01df6fbb09288407863b29ea3
S.M. Mozammal Hossain Imon <111281426+sm-mozammal@users.noreply.github.com>
2023-10-19 13:52:40 +0600

    second commit

--- 20_033_Data_Structure.c
+++ 20_033_Data_Structure.c
@@ -1,4 +1,39 @@
 #include<stdio.h>
 int main(){
-    printf("data structure");
+    printf("Welcome to Programing world");
+    int option;
+    do{
+        printf("Select any one \n");
+        printf("1. Linear Search \n");
+        printf("2. Binary Search \n");
+        printf("3. Bubble Short  \n");
+        printf("4. Insertion  \n");
+
+        printf("Select you option = ");
+        scanf("%d", &option);
+        switch(option){
+            case 0:{
+                break;
+            }
+            case 1: {
+                printf("linear search");
+                break;
+            }
+            case 2: {
+                printf("Binary search");
+                break;
+            }
+            case 3: {
+                printf("Bubble short");
+                break;
+            }
+            case 4: {
+                printf("Insertion");
+                break;
+            }
+            default:
+                printf("Please enter a valid option");
+                break;
+        }
+    } while(option != 0);
 }
